0a6e62a3e077fdadfe40a89a2e9f2d2581b5182f
yelucie <lucie.ye@reseau.eseo.fr>
2023-12-06 02:10:31 -0600

    Version 1.1

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,42 +1,68 @@
-import { Project } from '@/type';
-import React, { FunctionComponent } from 'react'
-import { AiFillGithub } from 'react-icons/ai';
+import { Project } from "@/type";
+import React, { FunctionComponent, useState } from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { MdClose } from "react-icons/md";
 
-const ProjectCard:FunctionComponent<{ project: Project }> = ({
-    project: { name, image_path, category, key_tech, deployed_url, github_url, description }
+const ProjectCard: FunctionComponent<{ project: Project }> = ({
+  project: {
+    name,
+    image_path,
+    category,
+    key_tech,
+    deployed_url,
+    github_url,
+    description,
+  },
 }) => {
+  const [showDetail, setShowDetail] = useState(false);
   return (
     <div>
-        <img src={ image_path } alt={ name } className='cursor-pointer' />
-        <p className='my-2 text-center'>{ name }</p>
-
-        <div className='grid md:grid-cols-2'>
-            <div>
-                <img src={ image_path } alt={ name } />
-                <div>
-                    <a href={github_url}>
-                        <AiFillGithub /><span>Github</span>
-                    </a>
-                    <a href={deployed_url}>
-                        <AiFillGithub /><span>Project</span>
-                    </a>
-                </div>
+      <img
+        src={image_path}
+        alt={name}
+        className="cursor-pointer"
+        onClick={() => setShowDetail(true)}
+      />
+      <p className="my-2 text-center">{name}</p>
+      { showDetail && (
+        <div className="absolute grid md:grid-cols-2 top-0 left-0 z-10 p-2 h-auto w-full gap-x-12 
+            text-black bg-gray-100 dark:text-white dark:bg-dark-100">
+          <div>
+            <img src={image_path} alt={name} />
+            <div className="flex justify-center my-4 space-x-3">
+              <a href={github_url} className="flex items-center px-4 py-2 space-x-3 txtlg
+              bg-gray-200 dark:bg-dark-200">
+                <AiFillGithub />
+                <span>Github</span>
+              </a>
+              <a href={deployed_url} className="flex items-center px-4 py-2 space-x-3 txtlg
+              bg-gray-200 dark:bg-dark-200">
+                <AiFillGithub />
+                <span>Project</span>
+              </a>
             </div>
-        </div>
+          </div>
 
-        <div>
-            <h2>{ name }</h2>
-            <h3>{ description }</h3>
-            <div>
-                {
-                    key_tech.map((tech) => (
-                        <span key={tech}>{ tech }</span>
-                    ))
-                }
+          <div>
+            <h2 className="mb-3 text-xl font-medium md:text-2xl">{name}</h2>
+            <h3 className="mb-3 font-medium">{description}</h3>
+            <div className="flex flex-wrap mt-5 space-x-2 text-sm tracking-wider">
+              {key_tech.map((tech) => (
+                <span key={tech} className="px-2 py-2 my-1 bg-gray-200 dark:bg-dark-200">
+                    { tech }
+                </span>
+              ))}
             </div>
+          </div>
+
+          <button onClick={() => setShowDetail(false)}
+            className="absolute top-3 right-3 rounded-full focus:outline-none bg-gray-200 dark:bg-dark-200">
+            <MdClose size={30} />
+          </button>
         </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,12 +4,12 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div className='p-4'>
+    <div className='px-5 py-2 overflow-y-scroll' style={{height: "65vh"}}>
         <nav>Navbar</nav>
 
-        <div className='grid grid-cols-12 gap-4 my-3'>
+        <div className='grid grid-cols-12 gap-4 my-3 relative'>
           { projects.map(project => (
-            <div className='col-span-12 p-2 sm:col-span-6 lg:col-span-4' key={ project.name }>
+            <div className='col-span-12 p-2 sm:col-span-6 lg:col-span-4 bg-gray-200 dark:bg-dark-200' key={ project.name }>
               <ProjectCard project={project} />
             </div>
           ))}
